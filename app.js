45f7fbe9edb8d9f8da5790183ee99c07447b054e
umairrr9 <umairulhaq_@outlook.com>
2020-05-07 23:49:30 +0100

    Connected to DB and created schema

---
+++ app.js
@@ -0,0 +1,37 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+var path = require("path");
+require("dotenv/config");
+
+const app = express();
+
+// Connect to DB
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useCreateIndex", true);
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.log("Connected to DB");
+});
+
+app.use(bodyParser.json());
+
+// Import Routes
+// const usersRoute = require("./routes/users");
+
+// app.use("/api/users/", usersRoute);
+
+// allows the app to use any necessary folders
+// app.use("/front-end", express.static(__dirname + "/front-end"));
+// app.use("/static", express.static(__dirname + "/static"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Success" }); //test
+});
+
+var port = process.env.PORT || 80;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 256,
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 40
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().pattern(new RegExp("[a-zA-Z0-9]{8,50}$")), // TASK for UMAIR: figure out regex pattern for passwords
+    username: Joi.string().min(1).max(40).pattern(new RegExp("[a-zA-Z0-9]"))
+  });
+
+  return schema.validate(user);
+}
+
+exports.User = User;
+exports.validate = validateUser;
+
+
