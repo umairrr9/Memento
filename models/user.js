526e55b2817ebee4599a954b71746cacb15fe77a
umairrr9 <umairulhaq_@outlook.com>
2020-05-14 19:13:55 +0100

    Created regex pattern and ID validation function

--- models/user.js
+++ models/user.js
@@ -29,17 +29,22 @@ const User = mongoose.model("User", userSchema);
 function validateUser(user) {
   const schema = Joi.object({
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().regex(RegExp("[a-z]")).required(), // TASK for UMAIR: figure out regex pattern for passwords
-    username: Joi.string().min(1).max(40).regex(RegExp("[a-zA-Z0-9]"))
+    //FIGURE OUT EXCLUSIONS FOR PASSWORD
+    password: Joi.string().min(8).max(50).pattern(new RegExp('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$')),
+    username: Joi.string().min(1).max(40).alphanum()
   });
 
   return schema.validate(user);
 }
 
+function validateID(_id) {
+  const schema = Joi.object({
+    _id: Joi.string().alphanum().length(24)
+  });
+
+  return schema.validate(_id);
+}
+
 exports.User = User;
 exports.validate = validateUser;
-
-
-// (?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9!?]{8, 50}
-
-// /(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[$@$!#.])[A-Za-zd$@$!%*?&.]{8,20}/ 
+exports.validateID = validateID;
