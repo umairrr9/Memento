6be7b89c45a1910161b212e103c364db8f4e97a0
umairrr9 <umairulhaq_@outlook.com>
2020-05-21 16:20:01 +0100

    created jwt functions

---
+++ static/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+function verifyJWT(token) {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded;
+  } catch (ex) {
+    //console.log(ex);
+    return null;
+  }
+}
+
+function generateJWT(_id, email, username) {
+  const token = jwt.sign({ _id, email, username }, process.env.JWT_SECRET);
+  return token;
+}
+
+module.exports = {verifyJWT, generateJWT};
