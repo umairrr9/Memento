29b714f0f8cc7b0ed25a5f5dbaca8a7fdc4c698d
umairrr9 <umairulhaq_@outlook.com>
2020-05-21 16:20:39 +0100

    created password functions

---
+++ static/hash.js
@@ -0,0 +1,17 @@
+const forge = require('node-forge');
+
+// Create the password hash
+function createHash(password, salt) {
+    var md = forge.md.sha256.create();
+    md.update(password + salt);
+    return md.digest().toHex();
+}
+
+// Return true if the password in the DB matches the inputted password
+function checkPassword(password, salt, databasePassword) {
+    return createHash(password, salt) === databasePassword;
+}
+
+// exports.createHash = createHash;
+// exports.checkPassword = checkPassword;
+module.exports = {createHash, checkPassword};
