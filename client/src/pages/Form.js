dbc1dc1d24e23a3f1f96b8e7b2b41cf962b4d074
harrisuddin <harris7001@gmail.com>
2020-06-11 22:49:47 +0100

    added logo to signup page

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import useForm from '../components/useForm';
 import NavBar from '../components/NavBar';
 import '../assets/test.css';
+import logo from '../assets/transparent.png';
 // import NavBar from '../components/NavBar'
 // https://upmostly.com/tutorials/using-custom-react-hooks-simplify-forms
 
@@ -12,12 +13,9 @@ const Form = () => {
   const passwordPattern2 = new RegExp('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$');
 
   useEffect(() => {
-    let isLogin = new URLSearchParams(location.search).get("isLogin");
-    
-    if (isLogin === "1" || isLogin === "0") {
-      setValues(values => ({ ...values, isLogin: isLogin}));
-    }
-  });
+    let isLogin = new URLSearchParams(window.location.search).get("isLogin") ? true : false;
+    setValues(values => ({ ...values, isLogin: isLogin}));
+  }, [setValues]);
 
   function login() {
     // validate the username, email and password
@@ -39,189 +37,77 @@ const Form = () => {
   }
 
   return (
-    <div class="bg-gray-100 md:mt-8 mx-auto max-w-2xl bg-white py-20 sm:px-12 lg:px-24 shadow-xl mb-24">
+    <div className="bg-gray-100 md:mt-8 mx-auto max-w-2xl bg-white py-16 sm:px-12 lg:px-24 shadow-xl mb-24">
+      <div className="flex justify-center mb-8">
+        <img src={logo} className="w-16 h-auto"/>
+        <h1 className="ml-6 self-center text-brandBlue-A font-bold text-xl">Memento</h1>
+      </div>
       <form onSubmit={handleSubmit}>
-        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-          {/* <div class="-mx-3 md:flex mb-6">
-          <div class="md:w-1/2 px-3 mb-6 md:mb-0">
-            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="username">
-              Username
-            </label>
-            <input name="username" class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" type="text" placeholder="Username"/>
-            <div>
-              <span class="text-red-500 text-xs italic">
-                Please fill out this field.
-              </span>
-            </div>
-          </div>
-          <div class="md:w-1/2 px-3">
-            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="title">
-              Title*
-            </label>
-            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="title" type="text" placeholder="Software Engineer"/>
-          </div>
-        </div> */}
-          {!isLogin && <div class="-mx-3 md:flex mb-6">
-            <div class="md:w-full px-3">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+          {!isLogin && <div className="-mx-3 md:flex mb-6">
+            <div className="md:w-full px-3">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                 Username
               </label>
               <input name="username" className={usernameError ? "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" : "shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"} type="text" placeholder="Username" onChange={handleChange} value={username || ""} required />
               {usernameError && <div>
-                <span class="text-red-500 text-xs italic">
+                <span className="text-red-500 text-xs italic">
                   {usernameError}
                 </span>
               </div>}
             </div>
           </div>}
-          <div class="-mx-3 md:flex mb-6">
-            <div class="md:w-full px-3">
+          <div className="-mx-3 md:flex mb-6">
+            <div className="md:w-full px-3">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                 Email
               </label>
               <input name="email" className={emailError ? "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" : "shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"}  type="email" placeholder="user@example.com" onChange={handleChange} value={email || ""} required />
               {emailError && <div>
-                <span class="text-red-500 text-xs italic">
+                <span className="text-red-500 text-xs italic">
                   {emailError}
               </span>
               </div>}
             </div>
           </div>
-          <div class="-mx-3 md:flex mb-6">
-            <div class="md:w-full px-3">
+          <div className="-mx-3 md:flex mb-6">
+            <div className="md:w-full px-3">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                 Password
             </label>
-              <input name="password" className={passwordError ? "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" : "shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"}  type="password" placeholder="*********************" onChange={handleChange} value={password || ""}/>
+              <input name="password" className={passwordError ? "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" : "shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"}  type="password" placeholder="*********************" onChange={handleChange} value={password || ""} required/>
               {passwordError && <div>
-                <span class="text-red-500 text-xs italic">
+                <span className="text-red-500 text-xs italic">
                   {passwordError}
                 </span>
               </div>}
             </div>
           </div>
-          {!isLogin &&<div class="-mx-3 md:flex mb-6">
-            <div class="md:w-full px-3">
-              <label className="block text-gray-700 text-sm font-bold mb-2"/*class="uppercase tracking-wide text-black text-xs font-bold mb-2"*/ htmlFor="confirmPassword">
+          {!isLogin &&<div className="-mx-3 md:flex mb-6">
+            <div className="md:w-full px-3">
+              <label className="block text-gray-700 text-sm font-bold mb-2"/*className="uppercase tracking-wide text-black text-xs font-bold mb-2"*/ htmlFor="confirmPassword">
                 Confirm Password
             </label>
-              <input name="confirmPassword" className={confirmPasswordError ? "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" : "shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"} /*class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"*/ type="password" placeholder="*********************" onChange={handleChange} value={confirmPassword || ""}/>
+              <input name="confirmPassword" className={confirmPasswordError ? "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" : "shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"} /*className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"*/ type="password" placeholder="*********************" onChange={handleChange} value={confirmPassword || ""} required/>
               {confirmPasswordError && <div>
-                <span class="text-red-500 text-xs italic">
+                <span className="text-red-500 text-xs italic">
                   {confirmPasswordError}
                 </span>
               </div>}
             </div>
           </div>}
-
-          {/* <div class="-mx-3 md:flex mb-2">
-          <div class="md:w-1/2 px-3 mb-6 md:mb-0">
-            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="location">
-              Location*
-            </label>
-            <div>
-              <select class="w-full bg-gray-200 border border-gray-200 text-black text-xs py-3 px-4 pr-8 mb-3 rounded" id="location">
-                <option>Abuja</option>
-                <option>Enugu</option>
-                <option>Lagos</option>
-              </select>
-            </div>
-          </div>
-          <div class="md:w-1/2 px-3">
-            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="job-type">
-              Job Type*
-            </label>
-            <div>
-              <select class="w-full bg-gray-200 border border-gray-200 text-black text-xs py-3 px-4 pr-8 mb-3 rounded" id="job-type">
-                <option>Full-Time</option>
-                <option>Part-Time</option>
-                <option>Internship</option>
-              </select>
-            </div>
-          </div>
-          <div class="md:w-1/2 px-3">
-            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="department">
-              Department*
-            </label>
-            <div>
-              <select class="w-full bg-gray-200 border border-gray-200 text-black text-xs py-3 px-4 pr-8 mb-3 rounded" id="department">
-                <option>Engineering</option>
-                <option>Design</option>
-                <option>Customer Support</option>
-              </select>
-            </div>
-          </div>
-        </div> */}
-          <div class="flex justify-between align-center mt-2">
-            {/* <div class="md:w-full px-3"> */}
-            {/* <button class="bg-brandBlue-A text-white font-bold py-2 px-4 border-b-4 hover:border-b-2 border-gray-500 hover:border-gray-100 rounded-full">
-              Button
-            </button> */}
+          <div className="flex justify-between align-center mt-2">
             <button className="bg-brandBlue-A hover:bg-brandBlue-B text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
               {isLogin ? "Login" : "Sign Up"}
             </button>
             <a className="inline-block self-center font-semibold text-sm text-brandBlue-A hover:text-brandBlue-B" onClick={handleIsLogin} href="#">
               {isLogin ? "Sign Up instead?" : "Login instead?"}
             </a>
-            {/* </div> */}
           </div>
         </div>
       </form>
     </div>
-
   );
-
-  // return (
-  //   // <div className="bg-gray-100">
-  //     <div className="h-screen bg-white border-gray-400 p-4 px-3 py-10 bg-gray-200 flex justify-center">
-  //     <div className="w-full max-w-xs md:max-w-sm m-auto">
-  //     <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-  //       {isLogin && <div className="mb-4">
-  //         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-  //           Username
-  //         </label>
-  //         <input className="input shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="username" placeholder="Username" onChange={handleChange} value={username || ""} required/>
-  //         <p className="text-red-500 text-xs italic">{usernameError}</p>
-  //       </div>}
-  //       <div className="mb-4">
-  //         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-  //           Email
-  //         </label>
-  //         <input className="input shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" placeholder="user@example.com" onChange={handleChange} value={email || ""} required/>
-  //         <p className="text-red-500 text-xs italic">{emailError}</p>
-  //       </div>
-  //       <div className="mb-4">
-  //         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-  //           Password
-  //         </label>
-  //         <input className="input shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" placeholder="******************" onChange={handleChange} value={password || ""} required />
-  //         <p className="text-red-500 text-xs italic">{passwordError}</p>
-  //       </div>
-  //       {isLogin && <div className="mb-6">
-  //         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
-  //           Confirm Password
-  //         </label>
-  //         <input className="input shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="confirmPassword" type="password" placeholder="******************" onChange={handleChange} value={confirmPassword || ""} required />
-  //         <p className="text-red-500 text-xs italic">{confirmPasswordError}</p>
-  //       </div>}
-  //       <div className="flex items-center justify-between">
-  //         <button className="bg-brandBlue-A hover:bg-brandBlue-B text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-  //           {isLogin ? "Sign Up" : "Login"}
-  //         </button>
-  //         <a className="inline-block align-baseline font-semibold text-sm text-brandBlue-A hover:text-brandBlue-B" onClick={handleIsLogin} href="#">
-  //           {isLogin ? "Login instead?" : "Sign Up instead?"}
-  //         </a>
-  //       </div>
-  //     </form>
-  //     {/* <p className="text-center text-gray-500 text-xs">
-  //       &copy;2020 Lidth Corp. All rights reserved.
-  //     </p> */}
-  //   </div>
-  //     </div>
-
-  //   // </div>
-
-  // );
 };
 
 export default Form;
