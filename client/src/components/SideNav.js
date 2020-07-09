6ec5e3d19924ec2015fa196c754e5467fb3eb1d3
harrisuddin <harris7001@gmail.com>
2020-07-09 17:11:21 +0100

    kind of fixed tree view

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -1,81 +1,61 @@
-import React, {useState} from 'react';
-import {Link} from "react-router-dom";
-
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function SideNav() {
-
   const tree = [
     {
-      title: 'Beverages',
+      title: "Beverages",
       id: 1,
       children: [
-        { title: 'Coke', id: 2, value: '500ml bottle ASDA' },
-        { title: 'Water', id: 3, value: '2L bottle Tesco'},
-        { title: 'Fanta', id: 4, children: [
-          {title: 'Fruit Twist', id: 5, value: 'small can ASDA'},
-          {title: 'Orange', id: 6, value: 'can tesco'}
-        ]}
-      ]
+        { title: "Coke", id: 2, value: "500ml bottle" },
+        { title: "Water", id: 3, value: "2L bottle" },
+        {
+          title: "Fanta",
+          id: 4,
+          children: [
+            { title: "Fruit Twist", id: 5, value: "small can" },
+            { title: "Orange", id: 6, value: "test" },
+          ],
+        },
+      ],
     },
     {
-      title: 'Todo',
+      title: "Todo",
       id: 7,
       children: [
-        { title: 'Go shopping', id: 8, value: 'Apples, milk and bread.'},
-        { title: 'Clean room', id: 9, value: 'Clean room.'}
-      ]},
-      {title: 'Books', id: 10, value: 'Maths'}
+        { title: "Go shopping", id: 8, value: "Apples, milk and bread." },
+        { title: "Clean room", id: 9, value: "Clean desk, monitor." },
+      ],
+    },
+    { title: "Books", id: 10, value: "Maths" },
   ];
-  //https://material-ui.com/components/tree-view/
 
-    return (
-
-      <div>
-        
-         <Tree tree={tree} level={0}/>
-         
-         
-         &rsaquo;
-         <br />
-         &#9658;
-
-         &#9656;
-         &#9662;
-
-
-      </div>
-
-    )
-    
+  return (
+    <div>
+      <TreeView tree={tree} level={0} />
+    </div>
+  );
 }
 
 // https://medium.com/@swatisucharita94/recursive-rendering-in-react-42666102eae2
 
-const Tree = ({tree, level}) => {
+const TreeView = ({ tree, level }) => {
+
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <TreeView level={level}>
-    {
-      tree.map(n => (
-        <React.Fragment key={n.id}>
-        <h2>{n.title}</h2>
-        {n.children && <Tree tree={n.children} level={level+1}/>}
+    <>
+      {tree.map((t) => (
+        <React.Fragment key={t.id}>
+          <h2 style={{ marginLeft: `${level / 2}rem` }} onClick={() => setOpen(!isOpen)}>
+            <span className={t.children ? "" : "hidden"}>{isOpen ? "⇒" : "⇓"} </span>{t.title}</h2>
+          {t.children && <div className={isOpen && "hidden"}>
+            <TreeView tree={t.children} level={level + 1}></TreeView>
+          </div>}
         </React.Fragment>
-      ))
-    }
-    </TreeView>
+      ))}
+    </>
   );
-}
-
-const TreeView = ({level, children}) => {
-
-  // const [isOpen, setOpen] = useState(level == 0);
-  
-  return (
-    <div style={{'marginLeft': `${level / 2}rem`}} /*className={isOpen ? "" : "hidden"} onClick={() => setOpen(!isOpen)}*/>
-      {children}
-    </div>
-  )
-}
+};
 
 export default SideNav;
--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -3,7 +3,7 @@ import useForm from '../components/useForm';
 import NavBar from '../components/NavBar';
 import '../assets/test.css';
 import logo from '../assets/transparent.png';
-import ErrorIcon from '@material-ui/icons/Error';
+// import ErrorIcon from '@material-ui/icons/Error';
 import { Link } from 'react-router-dom';
 // import NavBar from '../components/NavBar'
 // https://upmostly.com/tutorials/using-custom-react-hooks-simplify-forms
@@ -175,7 +175,7 @@ const Form = ({isLogin}) => {
           {response && <div className="bg-red-100 flex items-center border border-red-400 text-red-700 px-4 py-3 rounded relative mb-6" role="alert">
             {/* <strong class="font-bold">Holy smokes!</strong>
             <span class="block sm:inline">Something seriously bad happened.</span> */}
-            <span><ErrorIcon/></span>
+            {/* <span><ErrorIcon/></span> */}
             <p className="ml-2 inline">{response}</p>
             {/* <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
               <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
