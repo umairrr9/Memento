3d4b452711fa124c60238ce906c25aa26010fb7f
harrisuddin <harris7001@gmail.com>
2020-07-23 00:23:54 +0100

    added features to treeview and modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -16,14 +16,14 @@ export default function Modal({children, closeOnClick, saveOnClick, showModal, t
                   <h3 className="text-3xl font-semibold">
                     {title}
                   </h3>
-                  <button
+                  {/* <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={closeOnClick}
                   >
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
-                  </button>
+                  </button> */}
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
--- client/src/components/TreeView.js
+++ client/src/components/TreeView.js
@@ -1,66 +1,22 @@
 import React, { useState } from "react";
 
-// const TreeView = ({ tree, level }) => {
-//   const [isOpen, setOpen] = useState(false);
-
-//   return (
-//     <>
-//       {tree.map((t) => (
-//           <React.Fragment key={t.id}>
-//             <div
-//               className="flex items-center"
-//               style={{
-//                 marginLeft: `${level / 2}rem`,
-//                 cursor: `${t.children ? "pointer" : ""}`,
-//               }}
-//               onClick={() => {
-//                 if (t.children) setOpen(!isOpen);
-//               }}
-//             >
-//               <span className={t.children ? "" : "hidden"}>
-//                 {!isOpen ? (
-//                   <span className="text-xl">&#9656; </span>
-//                 ) : (
-//                   <span className="text-xl">&#9662; </span>
-//                 )}
-//               </span>
-//               <h2 className="py-1 border-b-2 truncate">{t.title}</h2>
-//             </div>
-//             {t.children && (
-//               <div className={!isOpen ? "hidden" : ""}>
-//                 <TreeView tree={t.children} level={level + 1}></TreeView>
-//               </div>
-//             )}
-//           </React.Fragment>
-//         ))}
-//     </>
-//   );
-// };
-
-// import React, { useState } from "react";
-
 const TreeView = ({ node, tree, level, setSelectedFolder }) => {
   const [isOpen, setOpen] = useState(node.parentId === null ? true : false);
   let children = tree.filter((n) => n.parentId === node.id);
-  // https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/modals/regular
-  // https://www.creative-tim.com/learning-lab/tailwind-starter-kit/login
 
   return (
     <>
       {node.title && (
-        <span
+        <div
           className="flex items-center"
           key={node.id}
           style={{
             marginLeft: `${level}rem`,
-            cursor: `${!node.noteId ? "pointer" : ""}`,
+            cursor: `pointer`,
           }}
           onClick={() => {
-            if (children.length >= 1) {
-              setOpen(!isOpen);
-            } else {
-              if (node.noteId) console.log(node.noteId);
-            }
+            setOpen(!isOpen);
+            if (node.noteId) console.log(node.noteId);
             if (!node.noteId) {
               if (setSelectedFolder) setSelectedFolder(node);
               console.log(node);
@@ -68,32 +24,40 @@ const TreeView = ({ node, tree, level, setSelectedFolder }) => {
           }}
         >
           {!node.noteId && (
-            <span>
+            <span className="text-xl">
               {!isOpen ? (
-                <span className="text-xl">&#9656; </span>
+                <span>&#9656; </span>
               ) : (
-                <span className="text-xl">&#9662; </span>
+                <span>&#9662; </span>
               )}
             </span>
           )}
           <h2 className="py-1 border-b-2 truncate">{node.title}</h2>
-        </span>
-      )}
-      {children.map((t) => (
-        <div className={!isOpen ? "hidden" : ""} key={t.id}>
-          <TreeView
-            node={t}
-            tree={tree}
-            level={level + 1}
-            setSelectedFolder={setSelectedFolder}
-          ></TreeView>
         </div>
-      ))}
+      )}
+      <div className={"" + (!isOpen ? "hidden" : "")}>
+        {children.length >= 1 ? (
+          children.map((t) => (
+            <React.Fragment key={t.id}>
+              <TreeView
+                node={t}
+                tree={tree}
+                level={level + 1}
+                setSelectedFolder={setSelectedFolder}
+              ></TreeView>
+            </React.Fragment>
+          ))
+        ) : (
+          !node.noteId && <h3
+          className="text-gray-700 text-sm my-1"
+            style={{
+              marginLeft: `${level + 1}rem`
+            }}
+          >No pages inside</h3>
+        )}
+      </div>
     </>
   );
 };
-
-// export default TreeView;
-
 
 export default TreeView;
