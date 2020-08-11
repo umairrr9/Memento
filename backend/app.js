1fdcd87b9d6314b97b2a5bb0dae169054ef720cf
harrisuddin <harris7001@gmail.com>
2020-08-11 18:42:29 +0100

    serve react app from backend

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const session = require("express-session");
+const session = require("cookie-session");
 const cookieParser = require("cookie-parser");
 var path = require("path");
 const {verifyJWT, generateJWT} = require('./static/auth');
@@ -22,6 +22,8 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
   console.log("Connected to DB");
 });
 
+// Serve the static files from the React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.use(bodyParser.json());
 
@@ -29,10 +31,27 @@ app.use(bodyParser.json());
 // Initialize cookie-parser to allow us access the cookies stored in the browser
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// Initialize express-session to allow us track the logged-in user across sessions
+// // Initialize express-session to allow us track the logged-in user across sessions
+// app.use(
+//   session({ secret: process.env.COOKIE_SECRET, cookie: { maxAge: 60000 } })
+// );
+
+// initialize express-session to allow us track the logged-in user across sessions.
 app.use(
-  session({ secret: process.env.COOKIE_SECRET, cookie: { maxAge: 60000 } })
+  session({
+    name: "memento-session",
+    secret: process.env.COOKIE_SECRET,
+    cookie: { maxAge: 300000 },
+  })
 );
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    req.session.user = {};
+  }
+  console.log(req.session.user);
+  next();
+});
 
 
 // Import Routes
@@ -41,19 +60,20 @@ const notesRoute = require("./routes/notes");
 
 
 // Verify the users JWT
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
 
-  const decoded = verifyJWT(req.cookies.user_jwt);
-  if (!decoded) {
-    res.clearCookie('user_jwt');
-    req.session.user = {};
-  } else {
-    console.log("JWT exists");
-    req.session.user = decoded;
-  }
+//   const decoded = verifyJWT(req.cookies.user_jwt);
+//   console.log(decoded);
+//   if (!decoded) {
+//     res.clearCookie('user_jwt');
+//     req.session.user = {};
+//   } else {
+//     console.log("JWT exists");
+//     req.session.user = decoded;
+//   }
 
-  next();
-});
+//   next();
+// });
 
 
 app.use("/api/users/", usersRoute); 
@@ -65,16 +85,19 @@ app.use("/api/notes/", notesRoute);
 // app.use("/static", express.static(__dirname + "/static"));
 
 
-// ROUTES
-app.get("/", (req, res) => {
-  res.json({ message: "Success" }); //test
-});
+// // ROUTES
+// app.get("/", (req, res) => {
+//   res.json({ message: "Success" }); //test
+// });
 
-// Route for handling 404 requests(unavailable routes)
-app.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!");
-});
+// // Route for handling 404 requests(unavailable routes)
+// app.use(function (req, res, next) {
+//   res.status(404).send("Sorry can't find that!");
+// });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+});
 
 // PORT
 var port = process.env.PORT || 80;
